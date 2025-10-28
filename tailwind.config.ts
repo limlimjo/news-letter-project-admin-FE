ffce4e2dd0ce803b2ec62ddb04c72ecf80cb0fb1
limlimjo <skyblue.777@daum.net>
2025-10-28 12:37:25 +0000

    feat: 글꼴 설정 추가

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Pretendard", "ui-sans-serif", "system-ui"],
+      },
+    },
+  },
+  plugins: [],
+};
